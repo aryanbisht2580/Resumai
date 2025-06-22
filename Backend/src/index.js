31f6d7fd38beabf603c07b1306aa85b9b7b8d8ac
AryanBisht1234 <driveescape2580@gmail.com>
2025-06-22 10:48:53 +0530

    solved backend integration issue

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,21 @@
 import app from "./app.js";
 import { connectDB } from "./db/index.js";
 import { config } from "dotenv";
+import axios from "axios";
+
 config();
 
-connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("Server is running on http://localhost:" + process.env.PORT);
+connectDB().then(()=>{
+  app.listen(process.env.PORT,()=>{
+    console.log("Server is running on http://localhost:"+process.env.PORT);
+
+    setInterval(async () => {
+      try{
+        const response=await axios.get("https://resumai-backend-bcv7.onrender.com");
+        console.log("Pinged backend successfully:", response.status);
+      }catch (error){
+        console.error("Error pinging backend:",error.message);
+      }
+    }, 4 * 60 * 1000); 
   });
 });
